54ec22775d317fd147c290bdfdf056ca5a290ee7
Hari Paudel <paudel.hari977@gmail.com>
2022-08-07 11:27:49 +0545

    Additional solution to box.js

--- box.js
+++ box.js
@@ -6,3 +6,19 @@ function getSize(width, height, depth) {
     let volume = width * height * depth;
     return [area, volume]
   }
+
+  //Additional Solutions
+  function getSize(width, height, depth){
+    var box = { width: width, height:height, depth:depth};
+    
+    return [getArea(box), getVolume(box)];
+  }
+  
+  
+  function getArea(box){
+    return (2 * (box.depth * box.height)) + (2 * (box.width * box.height)) + (2 * (box.width * box.depth));
+  }
+  
+  function getVolume(box){
+    return box.width * box.height * box.depth;
+  }
