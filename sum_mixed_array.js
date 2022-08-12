9534682e0239e2372dfe076ccf719afe7969068d
Hari Paudel <paudel.hari977@gmail.com>
2022-08-12 22:53:20 +0545

    commit with working code

---
+++ sum_mixed_array.js
@@ -0,0 +1,23 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+// My answer 
+function sumMix(x){
+    let sum = 0;
+   for (let i = 0; i < x.length; i++) {
+     sum += Number(x[i]);
+   }
+    return sum;
+  }
+
+
+// Other answers
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+  }
+
+
+
+  const sumMix=x=>x.reduce((a,b)=>+b+a,0)
