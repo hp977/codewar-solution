aa89a169ffaef45a7e8bab4a0ba5efea98e7efec
Hari Paudel <paudel.hari977@gmail.com>
2022-08-14 08:07:58 +0545

    answer

---
+++ bool_toStr.js
@@ -0,0 +1,18 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// My Answer
+function booleanToString(b){
+    return b.toString()
+    //your code here
+  }
+
+// Other Answer
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
+
+function booleanToString(b){
+  return String(b);
+}
