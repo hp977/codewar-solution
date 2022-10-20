4aafb4068ec5de49e6a3fb2ba996b57d815757c6
Hari Paudel <109645425+hp977@users.noreply.github.com>
2022-10-20 18:18:12 +0545

    commit with answer

---
+++ 8kyu/grasshopperDebugSayHello.js
@@ -0,0 +1,14 @@
+// Question 
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+// Example output:
+
+// Hello, Mr. Spock
+
+
+// My answer
+function sayHello(name) {
+    return 'Hello, ' + name 
+  }
+
+// Other answer
+const sayHello = name => `Hello, ${name}`;
