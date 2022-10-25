45741909711dc92a83a7f8de452d32d2a619501f
Hari Paudel <109645425+hp977@users.noreply.github.com>
2022-10-25 17:27:41 +0545

    Codewar solution

---
+++ 8kyu/countOddNumberBelowN.js
@@ -0,0 +1,13 @@
+// Question 
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+
+// My answer
+function oddCount(n) {
+    return Math.floor(n/2)
+  }
+
+// Other answer
+oddCount=_=>this['Math']['floor'](_/[-~[]+!!{}][~~{}])
