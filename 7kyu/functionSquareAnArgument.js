c2ad529d59a45ebdc3309d282096526088908430
Hari Paudel <109645425+hp977@users.noreply.github.com>
2022-11-14 15:35:55 +0545

    Codewar with solution

---
+++ 7kyu/functionSquareAnArgument.js
@@ -0,0 +1,14 @@
+// Question
+// Now you have to write a function that takes an argument and returns the square of it. 
+
+
+// My answer
+
+// num => square of num
+
+const square = (number) => number * number;
+
+// Other answer
+var squareOfNumber = function(a){
+    return a*a;
+  }
