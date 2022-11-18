ef1c9729d662aa5419fd203d7ba8f2a219204635
Hari Paudel <109645425+hp977@users.noreply.github.com>
2022-11-18 20:20:00 +0545

    Codewar solution

---
+++ 8kyu/returnToSanity.js
@@ -0,0 +1,15 @@
+// Question 
+// Fix the function to match the result.
+
+
+// My answer
+function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results;
+  }
+
+
+
+// Other answer
+const mystery = () => ({sanity: 'Hello'});
