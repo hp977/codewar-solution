90617afd2981d587d06c5e5c4b17c29a2cd2bb73
Hari Paudel <109645425+hp977@users.noreply.github.com>
2022-12-05 08:58:03 +0545

    codewar solution

---
+++ 7kyu/noOdditiesHere.js
@@ -0,0 +1,24 @@
+// Question 
+// Write a small function that returns the values of an array that are not odd. 
+// All values in the array will be integers. Return the good values in the order they are given. 
+
+
+
+// My answer
+function noOdds(values) {
+    return values.filter(x => x%2 == 0)
+}
+
+
+// Other answer
+function noOdds( values ){
+    // Return all non-odd values
+    var goodies = [];
+    for(var i=0; i<values.length;i++){
+        if(values[i]%2 == 0){
+          goodies.push(values[i]);
+        }
+    }
+    
+    return goodies;
+  }
