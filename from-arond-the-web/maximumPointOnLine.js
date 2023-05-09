1717ee511bf78e4b8c76c93325f227552653290a
Hari Paudel <paudel.hari977@gmail.com>
2023-05-09 17:35:39 +0545

    some changes

---
+++ from-arond-the-web/maximumPointOnLine.js
@@ -0,0 +1,10 @@
+// Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+function maxPointsOnLine () {
+
+}
+// Example:
+// console.log(maxPointsOnLine([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]))
+//  4
+
+
